d6729f3ed508610b4d69b1a7e87a6ae6daf67aa9
Asad Nawaz <asadnawaz@Asads-MacBook-Pro.local>
2019-11-06 09:51:32 +0100

    ....

---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Asad Nawaz on 13/04/2019.
+//  Copyright © 2019 The Asad Nawaz. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Asad Nawaz on 13/04/2019.
+//  Copyright © 2019 The Asad Nawaz. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
